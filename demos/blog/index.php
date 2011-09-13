9697a692568657cab4104390508a474f6e9b947a
Jed Galbraith <jed@delorum.com>
2011-09-13 09:16:33 -0600

    printing out $yii variable

--- demos/blog/index.php
+++ demos/blog/index.php
@@ -4,8 +4,13 @@
 $yii=dirname(__FILE__).'/../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
+print_r ($yii);
+exit;
+
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
+
+
